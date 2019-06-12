aacca40599dd76c303af4b16c05520a05d7310da
felops <felops@users.noreply.github.com>
2019-06-11 22:42:43 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,16 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+
+const app = new Koa()
+const router = new Router()
+
+router.get('/', (ctx, next) => {
+ ctx.body = 'Index!'
+});
+
+router.get('/koa', (ctx, next) => {
+ ctx.body = 'Koa!'
+});
+
+app.use(router.routes())
+app.listen(3000)
