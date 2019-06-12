0b070c2fae23578d93fe6ca52e0dd6fdacd3f1a5
felops <felops@users.noreply.github.com>
2019-06-12 15:42:07 -0300

    adição do mongoose para CRUD de Pessoa

--- __tests__/routes.js
+++ __tests__/routes.js
@@ -1,21 +1,47 @@
 const request = require('supertest')
 const server = require('../index.js')
 
+beforeAll(() => server.Pessoa.remove())
+
 afterAll(() => {
-  server.close();
-  console.log('servidor finalizado')
+  server.Pessoa.remove()
+  server.app.close()
 })
 
 describe('testando rotas', () => {
   test('GET /', async () => {
-    const response = await request(server).get('/');
-    expect(response.status).toEqual(200);
-    expect(response.text).toContain('Index!');
+    const response = await request(server.app).get('/')
+    expect(response.status).toEqual(200)
+    expect(response.text).toContain('Index!')
   })
 
   test('GET /koa', async () => {
-    const response = await request(server).get('/koa');
-    expect(response.status).toEqual(200);
-    expect(response.text).toContain('Koa!');
+    const response = await request(server.app).get('/koa')
+    expect(response.status).toEqual(200)
+    expect(response.text).toContain('Koa!')
+  })
+
+  test('POST /pessoa (1)', async () => {
+    const response = await request(server.app).post('/pessoa').send({ nome: 'Felipe', sobrenome: 'Lopes' })
+    expect(response.status).toEqual(200)
+    expect(response.text).toContain('Salvo!')
+  })
+
+  test('POST /pessoa (2)', async () => {
+    const response = await request(server.app).post('/pessoa').send({ nome: 'Bill', sobrenome: 'Gates' })
+    expect(response.status).toEqual(200)
+    expect(response.text).toContain('Salvo!')
+  })
+
+  test('GET /pessoa', async () => {
+    const response = await request(server.app).get('/pessoa')
+    expect(response.status).toEqual(200)
+    expect(response.text).toEqual('Felipe Lopes, Bill Gates')
+  })
+
+  test('GET /pessoa/ultimo', async () => {
+    const response = await request(server.app).get('/pessoa/ultimo')
+    expect(response.status).toEqual(200)
+    expect(response.text).toEqual('Último cadastro: Bill Gates')
   })
 })
--- index.js
+++ index.js
@@ -1,18 +1,70 @@
 const Koa = require('koa')
 const Router = require('koa-router')
+const mongoose = require('mongoose')
+const koaBody = require('koa-body')
 
 const app = new Koa()
 const router = new Router()
 
+mongoose.connect('mongodb://localhost:27017/koa', { useNewUrlParser: true })
+
+const Pessoa = mongoose.model("Pessoa", new mongoose.Schema({
+  nome: { type: String, required: true },
+  sobrenome: { type: String, required: true },
+  idade: { type: Number, required: false },
+  criado_em: { type: Date, default: Date.now }
+}))
+
 router.get('/', (ctx, next) => {
- ctx.body = 'Index!'
-});
+  ctx.body = 'Index!'
+})
 
 router.get('/koa', (ctx, next) => {
- ctx.body = 'Koa!'
-});
+  ctx.body = 'Koa!'
+})
 
+router.get('/pessoa', async (ctx, next) => {
+  try {
+    await Pessoa.find({}, (err, response) => {
+      let nomes = response.map(r => r.nome + ' ' + r.sobrenome)
+      ctx.body = nomes.join(', ')
+    })
+  } catch(e) {
+    console.log(e)
+  }
+})
+
+router.get('/pessoa/ultimo', async (ctx, next) => {
+  try {
+    await Pessoa.findOne({}, {}, { sort: { 'criado_em' : -1 } }, (err, response) => {
+      ctx.body = 'Último cadastro: ' + response.nome + ' ' + response.sobrenome
+    })
+  } catch(e) {
+    console.log(e)
+  }
+})
+
+router.post('/pessoa', async (ctx, next) => {
+  try {
+    let body = ctx.request.body
+
+    await Pessoa({
+      nome: body.nome,
+      sobrenome: body.sobrenome,
+      idade: 23
+    })
+      .save()
+      .then(data => ctx.body = data ? 'Salvo!' : 'Erro!')
+  } catch(e) {
+    console.log(e)
+  }
+})
+
+app.use(koaBody({ multipart: true }))
 app.use(router.routes())
 
-const server = app.listen(3000)
-module.exports = server
+const server = module.exports = {
+  app: app.listen(3000),
+  mongoose,
+  Pessoa
+}
